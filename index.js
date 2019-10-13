cd1d1b93ca2fea2ba6c522e1aca493982957efe4
Irina Kagan <irinagamb@gmail.com>
2019-10-13 22:38:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Add your code here
+
+function submitData(usersName,usersemail){
+  fetch("http://localhost:3000/userss", {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify({
+    name: "Irina",
+    email: "irina@fun.com"
+  })
+});
+}
