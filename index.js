572b2552b2f107c58e1ebb25eb5de98f4696a374
Irina Kagan <irinagamb@gmail.com>
2019-10-13 22:46:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,23 +1,13 @@
-// Add your code here
-
-let submitData = {
-  name: "Irina",
-  email: "irina@gmail.com"
-};
-
-let configObj = {
+function submitDate(name,email){
+  fetch("http://localhost:3000/users", {
   method: "POST",
   headers: {
     "Content-Type": "application/json",
     "Accept": "application/json"
   },
-  body: JSON.stringify(submitData)
-};
-
-fetch("http://localhost:3000/users", configObj)
-  .then(function(response) {
-    return response.json();
+  body: JSON.stringify({
+    name: "kagan",
+    email: "kagan@irina.com"
   })
-  .then(function(object) {
-    console.log(object);
-  });
+});
+}
